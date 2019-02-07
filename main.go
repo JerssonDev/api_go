f1eea34c865af8089387831f8136c814fa43a34e
Jersson Mendoza <jersson926@gmail.com>
2019-02-07 17:11:54 -0500

    Api Terminada

--- main.go
+++ main.go
@@ -5,39 +5,43 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
-type persona struct {
-	Nombre   string `json:"name"`
-	Edad     string `json:"age"`
-	Telefono string `json:"cel"`
+type person struct {
+	Id        int    `json:"id"`
+	Name      string `json:"name"`
+	Age       uint8  `json:"age"`
+	Cellphone int    `json:"cellphone"`
 }
 
-var personas []*persona
+var persons []*person
 
-func addAmigo(p *persona) {
-	personas = append(personas, p)
+func addFriend(p *person) {
+	persons = append(persons, p)
 }
 
 func main() {
 
-	http.HandleFunc("/raiz", search)
+	http.HandleFunc("/", homePage)
 	http.HandleFunc("/search", search)
-	http.HandleFunc("/all", allAmigos)
+	http.HandleFunc("/all", friends)
 	http.HandleFunc("/create", create)
+	http.HandleFunc("/update", update)
+	http.HandleFunc("/delete", delete)
 
 	log.Fatal(http.ListenAndServe(":8081", nil))
 
 }
 
-func raiz(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Bienvendido!")
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome!")
 	//fmt.Println("Punto Salida: raiz")
 }
 
-func allAmigos(w http.ResponseWriter, r *http.Request) {
+func friends(w http.ResponseWriter, r *http.Request) {
 
-	collection := &personas
+	collection := persons
 
 	//fmt.Println("Punto Salida: Todos los articulos")
 	json.NewEncoder(w).Encode(collection)
@@ -58,16 +62,21 @@ func create(w http.ResponseWriter, r *http.Request) {
 		}
 
 		fmt.Fprintf(w, "Metodo Post! Create = %v\n", r.PostForm)
+
+		id, _ := strconv.Atoi(r.FormValue("id"))
 		name := r.FormValue("name")
-		age := r.FormValue("age")
-		cel := r.FormValue("cel")
+		age, _ := strconv.Atoi(r.FormValue("age"))
+		cellphone, _ := strconv.Atoi(r.FormValue("cellphone"))
 
-		p := &persona{}
-		p.Nombre = name
-		p.Edad = age
-		p.Telefono = cel
+		p := &person{}
+		p.Id = id
+		p.Name = name
+		p.Age = uint8(age)
+		p.Cellphone = cellphone
 
-		addAmigo(p)
+		addFriend(p)
+
+		fmt.Fprintf(w, "Person created! = %v\n", r.PostForm)
 
 	default:
 		fmt.Fprintf(w, "Lo siento, los metodos soportados son GET y POST.")
@@ -76,10 +85,6 @@ func create(w http.ResponseWriter, r *http.Request) {
 }
 
 func search(w http.ResponseWriter, r *http.Request) {
-
-	//coll := &personas
-
-	//collection := &personas
 
 	if r.URL.Path != "/search" {
 		http.Error(w, "404 not found.", http.StatusNotFound)
@@ -95,22 +100,101 @@ func search(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		fmt.Fprintf(w, "Metodo Post! Search = %v\n", r.PostForm)
+		fmt.Fprintf(w, "Metodo Post! Search = %v\n\n", r.PostForm)
 		aux := r.FormValue("name")
-		fmt.Fprintf(w, "Name = %s\n", aux)
-		/*
-			s, _ = json.Marshal(collection)
+		collection := persons
 
-			for _, v := range p {
+		for _, v := range collection {
 
-				if p.Nombre == aux {
-					fmt.Fprintf(w, "Name = %s\n", p.Nombre)
-					fmt.Fprintf(w, "Age = %s\n", p.Edad)
-					fmt.Fprintf(w, "Cel = %s\n", p.Telefono)
+			if v.Name == aux {
 
-				}
+				fmt.Fprintf(w, "Id = %d\n", v.Id)
+				fmt.Fprintf(w, "Name = %s\n", v.Name)
+				fmt.Fprintf(w, "Age = %d\n", v.Age)
+				fmt.Fprintf(w, "Cellphone = %d\n\n", v.Cellphone)
+			}
+		}
 
-			}*/
+	default:
+		fmt.Fprintf(w, "Lo siento, los metodos soportados son GET y POST.")
+	}
+}
+
+func update(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Path != "/update" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
+
+	switch r.Method {
+
+	case "POST":
+
+		if err := r.ParseForm(); err != nil {
+			fmt.Fprintf(w, "ParseForm() err: %v", err)
+			return
+		}
+
+		fmt.Fprintf(w, "Metodo Post! Update = %v\n", r.PostForm)
+		id, _ := strconv.Atoi(r.FormValue("id"))
+		name := r.FormValue("name")
+		age, _ := strconv.Atoi(r.FormValue("age"))
+		cellphone, _ := strconv.Atoi(r.FormValue("cellphone"))
+
+		collection := persons
+
+		for _, v := range collection {
+
+			if v.Id == id {
+
+				v.Name = name
+				v.Age = uint8(age)
+				v.Cellphone = cellphone
+			}
+		}
+
+		fmt.Fprintf(w, "Updated person!")
+
+	default:
+		fmt.Fprintf(w, "Lo siento, los metodos soportados son GET y POST.")
+	}
+}
+
+func delete(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Path != "/delete" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
+
+	switch r.Method {
+
+	case "POST":
+
+		if err := r.ParseForm(); err != nil {
+			fmt.Fprintf(w, "ParseForm() err: %v", err)
+			return
+		}
+
+		fmt.Fprintf(w, "Metodo Post! Update = %v\n", r.PostForm)
+		id, _ := strconv.Atoi(r.FormValue("id"))
+
+		collection := persons
+
+		var auxP []*person
+
+		for _, v := range collection {
+
+			if v.Id != id {
+
+				auxP = append(auxP, v)
+			}
+		}
+
+		persons = auxP
+
+		fmt.Fprintf(w, "Person removed!")
 
 	default:
 		fmt.Fprintf(w, "Lo siento, los metodos soportados son GET y POST.")
